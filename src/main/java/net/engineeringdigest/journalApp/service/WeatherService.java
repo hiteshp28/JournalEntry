9b186394b85adb300e864460cc8f0383aa790372
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-02-02 11:15:10 +0530

    Added api to yml

--- src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -2,6 +2,7 @@ package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,8 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherService {
-    private static final String apiKey="9920cb76357bfa60fccd9b4e2125972a";
+    @Value("${weather.api.key}")
+    private String apiKey;
     private static final String API="http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
 
     @Autowired
